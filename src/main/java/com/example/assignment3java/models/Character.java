a68e36a7f4e8580e9dfc04da8256b3109a52ab73
Amanda Ståhlberg <stahlberg.amanda@hotmail.com>
2023-03-01 10:00:23 +0100

    add models for movie, character and franchise

--- src/main/java/com/example/assignment3java/models/Character.java
+++ src/main/java/com/example/assignment3java/models/Character.java
@@ -1,4 +1,25 @@
 package com.example.assignment3java.models;
 
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
 public class Character {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(length = 50, nullable = false)
+    private String name;
+    @Column(length = 50)
+    private String alias;
+    @Column(length = 10)
+    private String gender;
+    @Column(length = 2083)
+    private String picture;
+
+    
+
+    @ManyToMany(mappedBy = "characters")
+    private Set<Movie> movies;
 }
--- src/main/java/com/example/assignment3java/models/Franchise.java
+++ src/main/java/com/example/assignment3java/models/Franchise.java
@@ -1,4 +1,20 @@
 package com.example.assignment3java.models;
 
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
 public class Franchise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(length = 50, nullable = false)
+    private String name;
+    @Column(length = 2000)
+    private String description;
+
+    @OneToMany(mappedBy = "franchise")
+    private Set<Movie> movies;
+
 }
