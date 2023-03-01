effeab3210f48dd863b9eb0e290becfe3f2d055b
Amanda Ståhlberg <stahlberg.amanda@hotmail.com>
2023-03-01 09:37:53 +0100

    first commit

---
+++ src/main/java/com/example/assignment3java/Assignment3JavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignment3java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment3JavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment3JavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/assignment3java/Assignment3JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignment3java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment3JavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
